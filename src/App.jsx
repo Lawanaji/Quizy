4dc5db60e85334f565161c6dd9d1d2b1520db280
oritise1 <ikareemabiodun730@gmail.com>
2024-10-19 01:11:24 +0100

    check

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./Pages/LandingPage/Home"
 import Dashboard from "./Pages/Dashboard/Dashboard"
 import Login from "./Pages/auth/login/login"
 import QuestionPage from '../src/Componant/QustionPage/index';
-import AnswerPage from  '../src/Componant/AnswerPage/index'
+import AnswerPage from '../src/Componant/AnswerPage/index'
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -15,23 +15,23 @@ function App() {
 
   return (
     <>
-    <ToastContainer autoClose={2000} position="top-right" style={{fontSize: "0.8rem"}} />
-    <BrowserRouter>
-      <Routes>
-            <Route path="/" element={<Layout><Home /></Layout>} />
-        {/* {Auth} */}
-            <Route path="/auth/login" element={<Login />} />
-            <Route path="/question" element={<QuestionPage />} />
-        <Route path="/answers" element={<AnswerPage />} />
-            <Route path="dashboard" element={<Dashboard />}>
-              
-            
-            
-            </Route>
+      <ToastContainer autoClose={2000} position="top-right" style={{ fontSize: "0.8rem" }} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          {/* {Auth} */}
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/question" element={<QuestionPage />} />
+          <Route path="/answers" element={<AnswerPage />} />
+          <Route path="dashboard" element={<Dashboard />}>
 
-      </Routes>
-    
-    </BrowserRouter>
+
+
+          </Route>
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
